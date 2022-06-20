c97c7783f3f10f3dec1c34a31677721ba1f5a7b8
John Canero <johncanero.official@gmail.com>
2022-06-20 16:11:00 +0800

    Added Data Key in Button HTML and Updated JS Code

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-// Detecting Button / Key Press
-    var numberOfPianoKeyButtons = document.querySelectorAll("key").length;
-    for (var i = 0; i<numberOfPianoKeyButtons; i++) {
-        document.querySelectorAll(".key")[i].addEventListener("click", function() {
-            var buttonInnerHTML = this.innerHtml
+// Detecting Button Press
+// var numberOfPianoKeyButtons = document.querySelectorAll(".key").length;
+// for (var i = 0; i < numberOfPianoKeyButtons; i++) {
+//     document.querySelectorAll(".key")[i].addEventListener("click", function () {
+//         var buttonInnerHTML = this.innerHTML
+
+//         // audio
+//         makeSound (buttonInnerHTML);
+
+//         // animation
+//         buttonAnimation (buttonInnerHTML);
+
+//     });
+
+//     }
 
 
-            // audio
-            makeSound (buttonInnerHTML);
+// Stack Overflow
+var pianoKeyButtons = document.querySelectorAll(".key");
 
-            // animation
-            buttonAnimation (buttonInnerHTML);
+pianoKeyButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        var key = this.getAttribute('data-key');
 
-        });
-    }
+        // audio
+        makeSound (key);
+        // animation
+        buttonAnimation (key);
+    });
+})
 
 
+    // Detecting Keyboard Press
     document.addEventListener("keydown", function(event) {
-         // alert("Key was pressed!");
+        // alert("Key was pressed!");
         // console.log(event);
 
         // audio
@@ -24,75 +40,67 @@
 
         buttonAnimation(event.key);
 
-
     });
-
-    
-
-
-
 
 
     // Detecting Sound Audio
     function makeSound(key) {
 
-             // audio
+           // audio
             //    switch (buttonInnerHTML)
             switch (key) {
-            case "a":
-                var piano1 = new Audio('sounds/piano1.mp3');
-                piano1.play();
-            break;
-            
-            case "s":
-                var piano2 = new Audio('sounds/piano2.mp3');
-                piano2.play();
-            break;
-
-
-            case "d":
-                var piano3 = new Audio('sounds/piano3.mp3');
-                piano3.play();
-            break;
-
-
-            case "f":
-                var piano4 = new Audio('sounds/piano4.mp3');
-                piano4.play();
-            break;
-
-
-
-            case "h":
-                var piano5 = new Audio('sounds/piano5.mp3');
-                piano5.play();
-            break;
-
-
-            case "j":
-                var piano6 = new Audio('sounds/piano6.mp3');
-                piano6.play();
-            break;
-
-
-            case "k":
-                var piano7 = new Audio('sounds/piano7.mp3');
-                piano7.play();
-            break;
-
-            case "l":
-                var piano8 = new Audio('sounds/piano8.mp3');
-                piano8.play();
-            break;
-
-
+                case "a":
+                    var piano1 = new Audio('sounds/piano1.mp3');
+                    piano1.play();
+                break;
+                
+                case "s":
+                    var piano2 = new Audio('sounds/piano2.mp3');
+                    piano2.play();
+                break;
+    
+    
+                case "d":
+                    var piano3 = new Audio('sounds/piano3.mp3');
+                    piano3.play();
+                break;
+    
+    
+                case "f":
+                    var piano4 = new Audio('sounds/piano4.mp3');
+                    piano4.play();
+                break;
+    
+    
+    
+                case "h":
+                    var piano5 = new Audio('sounds/piano5.mp3');
+                    piano5.play();
+                break;
+    
+    
+                case "j":
+                    var piano6 = new Audio('sounds/piano6.mp3');
+                    piano6.play();
+                break;
+    
+    
+                case "k":
+                    var piano7 = new Audio('sounds/piano7.mp3');
+                    piano7.play();
+                break;
+    
+                case "l":
+                    var piano8 = new Audio('sounds/piano8.mp3');
+                    piano8.play();
+                break;
+    
+    
 
 
             default: console.log(buttonInnerHTML);
         }
     }
-
-
 
 
 
@@ -107,8 +115,6 @@
             activeButton.classList.remove('pressed');
         }, 100);
     }
-
-
 
 
 
