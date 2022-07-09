f962219eb7a6784530f681cfaa516853bdc6e5c6
John Canero <johncanero.official@gmail.com>
2022-07-09 17:59:06 +0800

    Add Music Project Start Button Sound

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// Start Button
+// Functions to play main button audio on click
+function playSound () {
+    const sound = document.getElementById("startButtonSound");
+    sound.play();
+    sound.addEventListener("ended", function() {
+        location.href = "piano.html";
+    });
+}
+
+
+// Let's Go Button
+// Functions to play main button audio on click
